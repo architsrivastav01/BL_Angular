3222ee9c8a5ff94e596f6cd80309b9321480cd9d
Archit Srivastava <archit.srivastav_cs21@gla.ac.in>
2025-03-24 22:48:02 +0530

    UC2_Event_Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent{
   title = 'HelloWorld';
-  imgUrl="../assets/BL_logo_square_jpg.jpg"
+  imgUrl="../assets/BL_logo_square_jpg.jpg";
+  url = "https://www.bridgelabz.com"
   ngOnInit(): void {
     this.title = "Hello from bridgelabz.";
+  }
+  onClick($event: any){
+    console.log("Save button is clicked!",$event);
+    window.open(this.url,"_blank");
   }
 }
