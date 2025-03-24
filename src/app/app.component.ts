40946017fea4509a44274a182dee95e958b2f383
Archit Srivastava <archit.srivastav_cs21@gla.ac.in>
2025-03-24 22:50:47 +0530

    UC4_TwoWaybinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,11 @@ export class AppComponent{
   title = 'HelloWorld';
   imgUrl="../assets/BL_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com"
+  userName: string= "";
   ngOnInit(): void {
     this.title = "Hello from bridgelabz.";
   }
-  onClick($event: any){
+  onClick($event: any) : void {
     console.log("Save button is clicked!",$event);
     window.open(this.url,"_blank");
   }
