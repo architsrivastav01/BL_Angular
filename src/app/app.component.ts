ca626a776d0f58ba50bc7c7a3408b406147700c6
Archit Srivastava <archit.srivastav_cs21@gla.ac.in>
2025-03-24 22:52:32 +0530

    UC4_TwoWaybinding_Regex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,22 @@ export class AppComponent{
   imgUrl="../assets/BL_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com"
   userName: string= "";
+  nameError: String ="";
   ngOnInit(): void {
     this.title = "Hello from bridgelabz.";
   }
   onClick($event: any) : void {
     console.log("Save button is clicked!",$event);
     window.open(this.url,"_blank");
+  }
+  onInput($event: any){
+    console.log("Change Event Occured!",$event.data);
+    const nameRegex =RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError ="";
+      return;
+
+    }
+    this.nameError ="Name is Incoorect!";
   }
 }
